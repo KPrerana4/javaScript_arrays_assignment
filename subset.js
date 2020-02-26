3890ba51baacca94416818668de1ee9e3883db76
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-02-26 15:00:57 +0530

    check whether a list2 is subset of list1

---
+++ subset.js
@@ -0,0 +1,25 @@
+function isSubset(list1, list2)
+{
+    for(let element of list2)
+    {
+        if(count(list2, element) != count(list1, element)){
+            return false;
+        }
+    }
+    return true;
+}
+
+function count(list, searchingElement)
+{
+    let count = 0;
+    for(let element of list)
+    {
+        if(element == searchingElement)
+            count++;
+    }
+    return count;
+}
+
+console.log(isSubset([1,2,3],[1,2]));
+console.log(isSubset([1,2,2,3],[1,2,2]));
+console.log(isSubset([1,2,3],[1,4]));
