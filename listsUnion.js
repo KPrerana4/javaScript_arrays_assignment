956a860d7c793313c6f2fa4e48c38185377f9c86
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-02-26 21:28:36 +0530

    change function name

--- listsUnion.js
+++ listsUnion.js
@@ -1,7 +1,7 @@
-function unionLists(list1, list2)
+function union(list1, list2)
 {
     return list1.concat(list2);
 }
 
-console.log(unionLists([1,2,2,3],[1,2,2,4,6,6]));
-console.log(unionLists([],[1,2]));
+console.log("union([1,2,2,3],[1,2,2,4,6,6])="+ union([1,2,2,3],[1,2,2,4,6,6]));
+console.log("union([],[1,2])= "+ union([],[1,2]));
