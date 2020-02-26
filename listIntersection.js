7f7e8a2411052bffa1f2019ac87b8ef4f6c6a0ca
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-02-26 21:40:57 +0530

    change for loop

--- listIntersection.js
+++ listIntersection.js
@@ -1,36 +1,37 @@
-function intersectionOfLists(list1, list2){
+function intersection(list1, list2){
     let result = [];
     for(let element of list1){
         let list1ElementCount = count(list1, element);
         let list2ElementCount = count(list2, element);
-        let leastCount = Math.min(list1ElementCount, list2ElementCount);
-        result = addElementToResult(leastCount, result, element);
+        let minimumCount = Math.min(list1ElementCount, list2ElementCount);
+        result = addElementToResult(minimumCount, result, element);
     }
     return result;
 }
 
-function count(list, searchingElement)
+function count(list, element)
 {
     let count = 0;
-    for(let element of list)
+    for(let index = 0; index < list.length; index++)
     {
-        if(element == searchingElement)
+        if(list[index] == element)
             count++;
     }
     return count;
 }
 
-function addElementToResult(leastCount, result, element)
+function addElementToResult(minimumCount, result, element)
 {
-    if(count(result,element) == leastCount){
+    if(count(result,element) == minimumCount){
         return result;
     }
-    for(let i = 0; i < leastCount; i++){
+    for(let i = 0; i < minimumCount; i++){
       result.push(element);
     }
     return result;
 }
 
-console.log("result:" + intersectionOfLists([1,6,6,3],[1,2,2,3,4,6,6]));
-console.log("result:" + intersectionOfLists([1,6,3,2,2],[1,2,2,3,4,6,6]));
-console.log("result:" + intersectionOfLists([],[1,2,3]));
+console.log("Intersection between lists:");
+console.log("[1,6,6,3],[1,2,2,3,4,6,6]: " + intersection([1,6,6,3],[1,2,2,3,4,6,6]));
+console.log("[1,6,3,2,2],[1,2,2,3,4,6,6]: " + intersection([1,6,3,2,2],[1,2,2,3,4,6,6]));
+console.log(intersection([],[1,2,3]));
