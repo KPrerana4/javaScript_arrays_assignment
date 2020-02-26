a1a6effa42155aafdc28f9ff38f845a322f954e6
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-02-26 22:20:40 +0530

    remove extra statements

--- dropNElements.js
+++ dropNElements.js
@@ -1,11 +1,8 @@
 function drop(list, n)
 {
-    if(n >= list.length)
-    {
-        return "Not possible";
-    }
     return list.slice(n);
 }
 
 console.log("drop([1,2,3,4,5],2) = " + drop([1,2,3,4,5],2));
-console.log("drop([1,2,3,4,5],6) = " + drop([1,2,3,4,5],6));
+console.log("drop([1,2,3,4,5],6) = ");
+console.log(drop([1,2,3,4,5],6));
