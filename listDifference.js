2ad9481f518ff2bf26079965dc826ccf5aae6fa8
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-02-26 14:41:12 +0530

    difference between two lists (list1 - list2)

---
+++ listDifference.js
@@ -0,0 +1,13 @@
+function difference(list1, list2)
+{
+    let result = [];
+    for(let element of list1){
+        if(!list2.includes(element)){
+            result.push(element);
+        }
+    }
+    return result;
+}
+
+console.log(difference([1,2,3], [1,2]));
+console.log(difference([1,2,2], [1,2]));
