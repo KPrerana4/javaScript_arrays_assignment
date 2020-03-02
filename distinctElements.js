e9f83b55ec71657422aab00b756956d4fb43c57e
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-03-02 15:40:20 +0530

    using every()

--- distinctElements.js
+++ distinctElements.js
@@ -1,25 +1,10 @@
 function isDistinct(list)
 {
-    for(let element of list)
-    {
-        if(count(list, element) != 1){
-            return false;
-        }
-    }
-    return true;
+    return list.every(function(element,index,list){
+        return !list.slice(index + 1).includes(element);
+    });
 }
 
-function count(list, element)
-{
-    let count = 0;
-    for(let element1 of list)
-    {
-        if(element1 == element)
-            count++;
-    }
-    return count;
-}
-
-console.log("isDistinct([1,2,3]) = " + isDistinct([1,2,3]));
+console.log("isDistinct([1,2,3,4]) = " + isDistinct([1,2,3,4]));
 console.log("isDistinct([1,2,3,1]) = " + isDistinct([1,2,3,1]));
 console.log("isDistinct([]) = " + isDistinct([]));
