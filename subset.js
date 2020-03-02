5f71685ea5135e2dd28fc188d06e8dabae7b5b9f
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-03-02 14:19:12 +0530

    using filter and every methods

--- subset.js
+++ subset.js
@@ -1,23 +1,15 @@
 function isSubset(list1, list2)
 {
-    for(let element of list2)
-    {
-        if(count(list2, element) != count(list1, element)){
-            return false;
-        }
-    }
-    return true;
+    return list2.every(function(element){
+        return count(list2, element) == count(list1, element);
+    },list1);
 }
 
 function count(list, element)
 {
-    let count = 0;
-    for(let index = 0; index < list.length; index++)
-    {
-        if(list[index] == element)
-            count++;
-    }
-    return count;
+    return list.filter(function(member){
+        return element == member;
+    },element).length;
 }
 
 console.log("isSubset([1,2,3], [1,2]) = " + isSubset([1,2,3], [1,2]));
