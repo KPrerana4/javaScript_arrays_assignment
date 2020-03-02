5c5cb33e6f16b1ead8bee66b494b6c6e0926bdda
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-03-02 13:59:41 +0530

    using filter

--- listDifference.js
+++ listDifference.js
@@ -1,12 +1,8 @@
 function difference(list1, list2)
 {
-    let result = [];
-    for(let element of list1){
-        if(!list2.includes(element)){
-            result.push(element);
-        }
-    }
-    return result;
+    return list1.filter(function(element){
+        return !this.includes(element);
+    },list2);
 }
 
 console.log("difference([1,2,3], [1,2]) = " + difference([1,2,3], [1,2]));
