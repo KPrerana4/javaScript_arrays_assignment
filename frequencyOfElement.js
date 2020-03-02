2e6c33c3d8d0fcad5a9b2a20f21e9765c2aedfff
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-03-02 15:55:23 +0530

    using filter()

--- frequencyOfElement.js
+++ frequencyOfElement.js
@@ -9,5 +9,10 @@ function count(list, element)
     return count;
 }
 
-console.log("count([1,2,3,4,1,2,1,6],2) : " + count([1,2,3,4,1,2,1,6],2));
-console.log("count([1,2,3,4,1,2,1,6],8) : " + count([1,2,3,4,1,2,1,6],8));
+function frequency(list, element)
+{
+    return list.filter((member) => member == element).length;
+}
+
+console.log("count([1,2,3,4,1,2,1,6,2,2],2) : " + frequency([1,2,3,4,1,2,1,6,2,2],2));
+console.log("count([1,2,3,4,1,2,1,6],8) : " + frequency([1,2,3,4,1,2,1,6],8));
