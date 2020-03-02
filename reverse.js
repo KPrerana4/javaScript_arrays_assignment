5bb8a4cd375b3fb2a8d7037efc9a855132e8053d
KPrerana4 <prerana.kannegundla@thoughtworks.com>
2020-03-02 14:28:23 +0530

    using every()

--- reverse.js
+++ reverse.js
@@ -1,36 +1,18 @@
-function isReverse(list1, list2){
-    let isEqual = false;
-    if(list1.length == list2.length){
-        list1 = reverseTheList(list1);
-        isEqual = areListsEqual(list1,list2);
-    }
-    return isEqual;
-}
-
-function reverseTheList(list1)
+function isReverse(list1, list2)
 {
-    let reverseList = [];
-    for(let index = list1.length - 1; index >= 0; index--)
+    if(list1.length == list2.length)
     {
-        reverseList.push(list1[index]);
+        return list1.every(function(element, index){
+                return list1[index] == list2[list2.length - index - 1];
+            },list2);
     }
-    return reverseList;
-}
-
-function areListsEqual(list1, list2)
-{
-    for(let index = 0; index < list1.length; index++){
-        if(list1[index] != list2[index]){
-            return false;
-        }
-    }
-    return true;
+    return false;
 }
 
 function main()
 {
-    let list1=[1,2,3];
-    let list2=[3,2,1];
+    let list1=[1,2,3,4];
+    let list2=[4,3,2,1];
     console.log("list1 = "+list1+"     list2 = "+list2);
     console.log("isReverse(list1,list2) = " + isReverse(list1,list2));
     console.log("isReverse(list2,list1) = " + isReverse(list2,list1));
